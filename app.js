a20a8c768a773561f46b357dfde7339a63d90294
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-09-27 13:59:11 +0200

    For anidado para ver la carpeta a la que pertenece

--- app.js
+++ app.js
@@ -8,19 +8,36 @@ var wrench = require("wrench");
 const app = express()
 const fs = require('fs');
 const Directorio = require('./clases/directorio');
+const { send } = require('process');
 
 // Variables
-const rutaRaiz = '//Serverdoc/e/z_Informatica/Z_Documentacio/'
+const rutaRaiz = '//Serverdoc/e/z_Informatica/Z_Documentacio/';
+let archivos = []
+let directorios = []
 var files = wrench.readdirSyncRecursive(rutaRaiz);
-    wrench.readdirRecursive(rutaRaiz, function (error, files) {
-});
+    wrench.readdirRecursive(rutaRaiz, function (error, files) {});
+files.forEach(file => {
+    if(fs.lstatSync((rutaRaiz + file)).isFile()) {
+        archivos.push(file)
+    } else {
+        directorios.push(file)
+    }
+})
+
+archivos.forEach(file => {
+    for(let i = 0; i < directorios.length; i++) {
+        if(file.includes(directorios[i])) {
+            console.log(file + " -> " + directorios[i])
+        }
+    }
+})
 
 // Config
 app.set('view engine', 'ejs')
 
 //HTTPs
 app.get('/', (req, res) => {
-    console.log(files)
+    res.send(files)
 })
 
 app.listen(3000)
